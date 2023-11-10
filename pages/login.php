967285b8270366142842b7d210e4d15b0bf54055
Lindorm236 <elielbjean@gmail.com>
2023-11-10 14:21:11 +0000

    Ajout formulaire dans login

--- pages/login.php
+++ pages/login.php
@@ -1,2 +1,13 @@
 <h2>Connexion</h2>
 <a href="../index.php">Retour</a>
+<form action="../results/loginResult.php" method="post">
+    <!-- Todo : Change type-->
+<label for="user_name">user_name</label>
+<input type="text" name="user_name" id="user_name" value="">
+
+<label for="pwd">mot de passe</label>
+<input type="text" name="pwd" id="pwd" value="">
+<button type="submit" >Se connecter</button>
+
+
+</form>

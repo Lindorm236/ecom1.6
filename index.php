aaa16e3198e5bed48835eb467cf8db47c601e0f9
Lindorm236 <elielbjean@gmail.com>
2023-11-10 13:40:10 +0000

    les liens

---
+++ index.php
@@ -0,0 +1,8 @@
+<h1>Accueil</h1>
+<a href="./pages/signUp.php">Enregistrement</a>
+<a href="./pages/login.php">Connexion</a>
+<?php
+
+
+
+?>
---
+++ pages/login.php
@@ -0,0 +1,2 @@
+<h2>Connexion</h2>
+<a href="../index.php">Retour</a>
